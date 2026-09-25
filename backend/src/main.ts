b43d8fd3038774916c17befe14065719ad1bb84a
tiendat07525 <tiendat07525.work@gmail.com>
2026-09-25 08:52:06 +0700

    update allow url

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,13 +10,23 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
       transform: true
     }));
+
+  const allowedOrigins = ["http://localhost:3000", "https://tiendat75.id.vn", "http://tiendat75.id.vn"] 
   app.enableCors({
-    origin: "http://localhost:3000",
-    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTION'],
+    origin: (origin, callback)=>{
+      if(!origin || allowedOrigins.includes(origin)){
+        callback(null, true);
+      } else{
+        callback(new Error('Blocked by CORS'));
+      }
+    },
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
-  await app.listen(process.env.PORT ?? 4000);
-  console.log(`Server đang chạy trên ${await app.getUrl()}`)
+
+  const port = process.env.PORT ?? 4000;
+  await app.listen(port, '0.0.0.0');
+  console.log(`Server đang chạy trên: http://0.0.0.0:${port}`)
 }
 bootstrap();
